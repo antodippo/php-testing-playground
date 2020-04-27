c595b897c17f72c96dbbdd254c9ab28a340a44f0
Antonello D'Ippolito <antonellodippolito@gmail.com>
2020-04-27 11:58:38 +0200

    Add property testing dependencies

--- tests/NuclearReactorTest.php
+++ tests/NuclearReactorTest.php
@@ -8,11 +8,11 @@ use PHPUnit\Framework\TestCase;
 
 class NuclearReactorTest extends TestCase
 {
-    public function testIsDangerous()
+    public function testIsDangerous(): void
     {
         $nuclearReactor = new NuclearReactor();
         $this->assertFalse($nuclearReactor->isDangerous(500));
         $this->assertTrue($nuclearReactor->isDangerous(2000));
-        // $this->assertFalse($nuclearReactor->isDangerous(1000));
+        $this->assertFalse($nuclearReactor->isDangerous(1000));
     }
 }
